139363f5c03ab254cdbf3617fd23fe3c6a111f49
George Jegon <george_jegon@hotmail.com>
2015-05-10 04:07:47 -0300

    Created Server and Client classes and instance one Client to test

---
+++ src/Client.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client {
+	private Socket client;
+	
+	public Client(){
+		initClient();
+	}
+	
+	private void initClient(){
+		try{
+			client = new Socket("127.0.0.1", 3322);
+			Scanner keyboard = new Scanner(System.in);
+			PrintStream output = new PrintStream(client.getOutputStream());
+			
+			while(keyboard.hasNextLine()){
+				output.println(keyboard.nextLine());
+			}
+		
+			output.close();
+			keyboard.close();
+		}catch(IOException exception){
+			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, exception);
+		}
+	}
+
+}
---
+++ src/Server.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Server {
+	public static void main(String args[]){
+		try {
+			ServerSocket server = new ServerSocket(3322);
+			System.out.println("Servidor iniciado na porta 3322");
+			
+			Socket client = server.accept();
+			System.out.println("Cliente conectado do IP" + client.getInetAddress().getHostAddress());
+			
+			Scanner entry = new Scanner(client.getInputStream());
+			
+			while(entry.hasNextLine()) {
+				System.out.println(entry.nextLine());
+			}
+			
+			entry.close();
+			server.close();
+		}catch(IOException exception){
+			Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, exception);
+		}		
+	}
+}
