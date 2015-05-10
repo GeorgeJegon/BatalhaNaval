74539e74b2035f24621bfbefd15ff60b18ae21f6
George Jegon <george_jegon@hotmail.com>
2015-05-10 19:44:55 -0300

    Applying new identation format

--- src/Client.java
+++ src/Client.java
@@ -6,28 +6,28 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Client {
-	private Socket client;
+  private Socket client;
 
-	public Client() {
-		initClient();
-	}
+  public Client() {
+    initClient();
+  }
 
-	private void initClient() {
-		try {
-			client = new Socket("127.0.0.1", 3322);
-			Scanner keyboard = new Scanner(System.in);
-			PrintStream output = new PrintStream(client.getOutputStream());
+  private void initClient() {
+    try {
+      client = new Socket("127.0.0.1", 3322);
+      Scanner keyboard = new Scanner(System.in);
+      PrintStream output = new PrintStream(client.getOutputStream());
 
-			while (keyboard.hasNextLine()) {
-				output.println(keyboard.nextLine());
-			}
+      while (keyboard.hasNextLine()) {
+        output.println(keyboard.nextLine());
+      }
 
-			output.close();
-			keyboard.close();
-		} catch (IOException exception) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null,
-					exception);
-		}
-	}
+      output.close();
+      keyboard.close();
+    } catch (IOException exception) {
+      Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null,
+          exception);
+    }
+  }
 
 }
