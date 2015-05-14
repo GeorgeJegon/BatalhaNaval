92b1e5a0c0f7c943e284456f0c6dddd895e43c66
George Jegon <george_jegon@hotmail.com>
2015-05-14 03:39:03 -0300

    Put weapon at distinct cells (with validation)

--- src/Principal.java
+++ src/Principal.java
@@ -1,96 +1,70 @@
+import components.orientations.*;
 import components.weapons.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.Random;
 
 public class Principal {
+  static Properties prop;
+  static ArrayList<Orientation> listOrientations = new ArrayList<Orientation>();
 
   /**
    * @param args
    * @throws ClassNotFoundException
    * @throws IllegalAccessException
    * @throws InstantiationException
- * @throws IOException 
+   * @throws IOException
    */
   public static void main(String[] args) throws ClassNotFoundException,
       InstantiationException, IllegalAccessException, IOException {
     // TODO Auto-generated method stub
-	Properties prop = getProp();
-	int gridSize = Integer.parseInt(prop.getProperty("gridSize"));
+    prop = getProp();
+    int gridSize = Integer.parseInt(prop.getProperty("gridSize"));
 
     Weapon[][] grid = new Weapon[gridSize][gridSize];
-    ArrayList<Weapon> listWeapons = new ArrayList<Weapon>();
 
+    ArrayList<Weapon> listWeapons = new ArrayList<Weapon>();
     populateWeapons(listWeapons, Aerocarrier.class, 5);
     populateWeapons(listWeapons, Submarine.class, 3);
     populateWeapons(listWeapons, TorpedoBoat.class, 10);
     populateWeapons(listWeapons, BattleShip.class, 3);
 
+    listOrientations.addAll(Arrays.asList(new Right(), new Up(), new Left(),
+        new Bottom()));
+
     addWeaponToGrid(grid, listWeapons);
   }
-  
+
   public static Properties getProp() throws IOException {
-	  Properties prop = new Properties();
-	  FileInputStream file = new FileInputStream("./src/config.properties");
-	  prop.load(file);
-	  return prop;
+    Properties prop = new Properties();
+    FileInputStream file = new FileInputStream("./src/config.properties");
+    prop.load(file);
+    return prop;
   }
 
- 
   public static void addWeaponToGrid(Weapon[][] grid,
       ArrayList<Weapon> listWeapon) {
     Random r = new Random();
     int[] position = new int[2];
+    int cells;
+    Orientation orientation;
 
     for (Weapon weapon : listWeapon) {
-      position[0] = r.nextInt(10);
-      position[1] = r.nextInt(10);
-      System.out.println("OI");
-      checkEmptyCells(grid, weapon, position);
+      cells = weapon.getCellsOccupation();
+      do {
+        position[0] = r.nextInt(100);
+        position[1] = r.nextInt(100);
+        orientation = listOrientations.get(r.nextInt(4));
+      } while(!orientation.checkEmptyCells(grid, cells, position));
+      orientation.fillCells(grid, weapon, position);
+      System.out.println("\nEstou nas posições:\n");
+      weapon.listPosition();
     }
   }
-
-  public static void checkEmptyCells(Weapon[][] grid, Weapon weapon,
-      int[] position) {
-    int x = position[0], y = position[1], cells = weapon.getCellsOccupation();
-
-    if (grid[x][y] == null) {
-      // ArrayOutOfBounds - Right 
-      if ((y + cells) < 100 && walkThroughGrid(grid, position, 0, 1, cells)) {
-        System.out.println("Pode Direita");
-      }
-
-      // ArrayOutOfBounds - Left
-      if ((y - cells) >= 0 && walkThroughGrid(grid, position, 0, -1, cells)) {
-        System.out.println("Pode Esquerda");
-      }
-
-      // ArrayOutOfBounds - Bottom
-      if ((x + cells) < 100 && walkThroughGrid(grid, position, 1, 0, cells)) {
-        System.out.println("Pode para Baixo");
-      }
-
-      // ArrayOutOfBounds - Top
-      if ((x - cells) >= 0 && walkThroughGrid(grid, position, -1, 0, cells)) {
-        System.out.println("Pode para Cima");
-      }
-
-    }
-  }
-
-  public static boolean walkThroughGrid(Weapon[][] grid,int [] position, int dx, int dy,
-      int stop) {
-    int x = position[0], y = position[1];
-    for (int i = 1; i <= stop; i++) {
-      if (grid[x + (dx * i)][y + (dy * i)] != null) {
-        return false;
-      }
-    }
-    return true;
-  }
-
+  
   public static void populateWeapons(ArrayList<Weapon> listWeapons, Class c,
       int quantity) throws InstantiationException, IllegalAccessException {
     while (quantity-- > 0) {
