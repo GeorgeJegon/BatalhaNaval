02abe90edf882764c5829955d884efb9f961f975
George Jegon <george_jegon@hotmail.com>
2015-05-25 17:45:15 -0300

    Created Player class and actions

---
+++ src/components/player/Player.java
@@ -0,0 +1,41 @@
+package components.player;
+
+public class Player {
+	private int remaingShots;
+	private int points;
+	private String name = new String();
+	
+	public Player(String name) {
+		this.remaingShots = 20;
+		this.points = 0;
+		this.name = name;
+	}
+	
+	public void addPoints (int points) {
+		this.points += points;
+	}
+
+	public int getRemaingShots() {
+		return remaingShots;
+	}
+
+	public void setRemaingShots(int remaingShots) {
+		this.remaingShots = remaingShots;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
