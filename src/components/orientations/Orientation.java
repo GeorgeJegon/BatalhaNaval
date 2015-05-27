fe520c791bd674dbb61a6bf6f9c99e078a1a2786
George Jegon <george_jegon@hotmail.com>
2015-05-27 02:39:01 -0300

    Now using Grid Object as param instead of Weapon[][]

--- src/components/orientations/Orientation.java
+++ src/components/orientations/Orientation.java
@@ -1,11 +1,12 @@
 package components.orientations;
 
+import components.grid.Grid;
 import components.weapons.Weapon;
 
 public abstract class Orientation {
   protected int dx = 0, dy = 0;
 
-  public boolean checkEmptyCells(Weapon[][] grid, int iterations,
+  public boolean checkEmptyCells(Grid grid, int iterations,
       int[] initialPosition) {
     int x, y;
 
@@ -17,34 +18,34 @@ public abstract class Orientation {
       x = initialPosition[0] + (this.dx * i);
       y = initialPosition[1] + (this.dy * i);
 
-      if (validPosition(grid, x, y) && grid[x][y] != null) {
+      if (validPosition(grid, x, y) && !grid.get(x, y).isEmpty()) {
         return false;
       }
     }
     return true;
   }
 
-  protected boolean validPosition(Weapon[][] grid, int x, int y) {
-    int limitX = grid.length, limitY = grid[0].length;
-    if ((x >= 0 && x < limitX) && (y >= 0 && y < limitY)) {
+  protected boolean validPosition(Grid grid, int x, int y) {
+    int limit = grid.getGridSize();
+    if ((x >= 0 && x < limit) && (y >= 0 && y < limit)) {
       return true;
     }
     return false;
   }
 
-  public void fillCells(Weapon[][] grid, Weapon weapon, int[] initialPosition) {
+  public void fillCells(Grid grid, Weapon weapon, int[] initialPosition) {
     int iterations = weapon.getCellsOccupation();
  
     for (int i = 0; i < iterations; i++) {
       int[] position = new int[2];
       position[0] = initialPosition[0] + (this.dx * i);
       position[1] = initialPosition[1] + (this.dy * i);
-      grid[position[0]][position[1]] = weapon;
+      grid.get(position[0], position[1]).setContent(weapon);
       weapon.addPosition(position);
     }
   }
 
-  protected boolean validBounds(Weapon[][] grid, int iterations, int[] position) {
+  protected boolean validBounds(Grid grid, int iterations, int[] position) {
     iterations--;
     int maxIndexOfX = (position[0] + (this.dx * iterations)), maxIndexOfY = (position[1] + (this.dy * iterations));
     return validPosition(grid, maxIndexOfX, maxIndexOfY);
